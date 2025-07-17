69d469d68c0696a721e670281f29607de43ed77d
linozen <linus@sehn.dev>
2025-07-17 07:42:01 +0200

    fix: ollama host url

--- concisum/config.py
+++ concisum/config.py
@@ -2,10 +2,9 @@ import os
 from pydantic_ai.models.openai import OpenAIModel
 from pydantic_ai.providers.openai import OpenAIProvider
 
-
 model = OpenAIModel(
     os.getenv("OLLAMA_MODEL", "qwen3:8b"),
     provider=OpenAIProvider(
-        base_url=os.getenv("OLLAMA_HOST", "http://localhost:11434"), api_key="ollama"
+        base_url=os.getenv("OLLAMA_HOST", "http://localhost:11434/v1"), api_key="ollama"
     ),
 )
